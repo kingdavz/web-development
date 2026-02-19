6d36b528431c9a67a9b2751cec428095d7238b9b
etim-ipee <imaikopelijah001@gmail.com>
2026-02-19 14:40:09 +0100

    website backend

--- 0x02/1-main.py
+++ 0x02/1-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 """an entry point to flask app"""
 from flask import Flask, render_template, request
-from utl import read_student, write_student
+from utl import read_student, write_student, verification
 
 app = Flask(__name__)
 
@@ -12,18 +12,31 @@ def index():
 
 @app.route("/login", methods=[ "POST", "GET"], strict_slashes=False)
 def login():
-    return render_template("login.html")
+    username= request.form.get("username")
+    password= request.form.get("password")
+    res= {}
+    if username and password:
+        if verification(Username=username, Password=password):
+            res['message'] = "student login sucessful"
+        else:
+            res['message'] = "student not found"
+    return render_template("login.html",data=res)
 
 @app.route("/signup", methods={"POST", "GET"}, strict_slashes=False)
 def register():
-    data = request.get_data()
-    res= {}
-    if read_student(data):
-        res["message"] = "student already exists"
-    else:
-        write_student(data)
-        res["message"] = "student created sucessfully"
-    return render_template("signup.html", **res)
+    email = request.form.get("email")
+    username = request.form.get("username")
+    fullname = request.form.get("fullname")
+    password = request.form.get("password")
+    res = {}
+    if email and username and fullname and password:
+        if read_student(Email=email, Username=username):
+            res["message"] = "student already exists"
+        else:
+            write_student(Email=email, Username=username, Fullname=fullname, Password=password)
+            res["message"] = "student created sucessfully"
+
+    return render_template("signup.html", data=res)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- 0x02/utl.py
+++ 0x02/utl.py
@@ -3,27 +3,41 @@
 import csv
 import os
 
-def read_student(data):
+def read_student(**kwargs):
     """a function that read student data from csv file"""
     try:
         with open("students.csv", "r", encoding= "utf-8 ") as f:
             reader = csv.DictReader(f)
             for row in reader:
-                if row["email"] == data.get("email").strip():
+        
+                if row['Email'] == kwargs['Email'] or row['Username'] == kwargs['Username']:
                     return True
         return False
     except FileNotFoundError:
         return False
     
-def write_student(data):
+def write_student(**kwargs):
     """A function that write student data into csv file"""
     try:
         with open("students.csv", "w+", encoding="utf-8") as f:
-            fieldnames = data.keys()
+            fieldnames = kwargs.keys()
             writer = csv.DictWriter(f, fieldnames=fieldnames)
             writer.writeheader()
-            writer.writerow(data)
+            writer.writerow(kwargs)
     except Exception:
         with open("students.csv", "a") as f:
-            f.write(data.decode("utf-8") + "\n")
+            f.write(kwargs)
     return True
+
+def verification(**kwargs):
+    """a function that verifies content in a file"""
+    try:
+        with open("students.csv", "r", encoding= "utf-8 ") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+            
+                if row['Username'] == kwargs['Username'] and row['Password'] == kwargs['Password']:
+                    return True
+        return False
+    except FileNotFoundError:
+        return False
